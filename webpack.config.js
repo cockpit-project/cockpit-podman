3f7ac60374974b381bfc43fa118b973944751b53
Dominik Perpeet <dperpeet@redhat.com>
2017-06-19 11:49:25 +0200

    build: Update webpack config
    Make building more convenient, add linting and distribute html files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,113 @@
 const path = require("path");
+const copy = require("copy-webpack-plugin");
+const fs = require("fs");
+
+/* These can be overridden, typically from the Makefile.am */
+const srcdir = (process.env.SRCDIR || __dirname) + path.sep + "src";
+const builddir = (process.env.SRCDIR || __dirname);
+const distdir = builddir + path.sep + "dist";
+const section = process.env.ONLYDIR || null;
+
+var info = {
+    entries: {
+        "index": [
+            "./index.js"
+        ]
+    },
+    files: [
+        "index.html"
+    ]
+};
+
+var output = {
+    path: distdir,
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+};
+
+/*
+ * Note that we're avoiding the use of path.join as webpack and nodejs
+ * want relative paths that start with ./ explicitly.
+ *
+ * In addition we mimic the VPATH style functionality of GNU Makefile
+ * where we first check builddir, and then srcdir.
+ */
+
+function vpath(/* ... */) {
+    var filename = Array.prototype.join.call(arguments, path.sep);
+    var expanded = builddir + path.sep + filename;
+    if (fs.existsSync(expanded))
+        return expanded;
+    expanded = srcdir + path.sep + filename;
+    return expanded;
+}
+
+/* Qualify all the paths in entries */
+Object.keys(info.entries).forEach(function(key) {
+    if (section && key.indexOf(section) !== 0) {
+        delete info.entries[key];
+        return;
+    }
+
+    info.entries[key] = info.entries[key].map(function(value) {
+        if (value.indexOf("/") === -1)
+            return value;
+        else
+            return vpath(value);
+    });
+});
+
+/* Qualify all the paths in files listed */
+var files = [];
+info.files.forEach(function(value) {
+    if (!section || value.indexOf(section) === 0)
+        files.push({ from: vpath("src", value), to: value });
+});
+info.files = files;
+
+var plugins = [
+    new copy(info.files)
+];
 
 module.exports = {
-    entry: ["./src/index.js"],
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "build")
-    },
+    entry: info.entries,
+    output: output,
     module: {
         rules: [
             {
-              exclude: /node_modules/,
-              loader: 'babel-loader',
-              test: /\.js$/
+                enforce: 'pre',
+                exclude: /node_modules/,
+                loader: 'jshint-loader',
+                test: /\.js$/
+            },
+            {
+                enforce: 'pre',
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                test: /\.jsx$/
+            },
+            {
+                enforce: 'pre',
+                exclude: /node_modules/,
+                loader: 'jshint-loader?esversion=6',
+                test: /\.es6$/
+            },
+            {
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                test: /\.js$/
+            },
+            {
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                test: /\.jsx$/
+            },
+            {
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                test: /\.es6$/
             }
         ]
-    }
+    },
+    plugins: plugins
 }
