6fa622e7bee9d818cb5169ded110b8811b2c70b2
Martin Pitt <martin@piware.de>
2018-07-03 09:20:12 +0200

    Import code from podman demo

--- src/app.jsx
+++ src/app.jsx
@@ -22,24 +22,108 @@ import cockpit from 'cockpit';
 import React from 'react';
 import './app.scss';
 
-const _ = cockpit.gettext;
+/***
+ * varlink protocol helpers
+ * https://github.com/varlink/varlink.github.io/wiki
+ */
+
+const encoder = cockpit.utf8_encoder();
+const decoder = cockpit.utf8_decoder(true);
+
+const PODMAN = { unix: "/run/io.projectatomic.podman" };
+
+/**
+ * Do a varlink call on an existing channel. You must *never* call this
+ * multiple times in parallel on the same channel! Serialize the calls or use
+ * `varlinkCall()`.
+ *
+ * Returns a promise that resolves with the result parameters or rejects with
+ * an error message.
+ */
+function varlinkCallChannel(channel, method, parameters) {
+    return new Promise((resolve, reject) => {
+        function on_close(event, options) {
+            reject(options.problem || options);
+        }
+
+        function on_message(event, data) {
+            channel.removeEventListener("message", on_message);
+            channel.removeEventListener("close", on_close);
+
+            // FIXME: support answer in multiple chunks until null byte
+            if (data[data.length - 1] != 0) {
+                reject("protocol error: expecting terminating 0");
+                return;
+            }
+
+            var reply = decoder.decode(data.slice(0, -1));
+            var json = JSON.parse(reply);
+            if (json.error)
+                reject(json.error)
+            else if (json.parameters) {
+                // debugging
+                console.log("varlinkCall", method, "→", JSON.stringify(json.parameters));
+                resolve(json.parameters)
+            } else
+                reject("protocol error: reply has neither parameters nor error: " + reply);
+        }
+
+        channel.addEventListener("close", on_close);
+        channel.addEventListener("message", on_message);
+        channel.send(encoder.encode(JSON.stringify({ method, parameters: (parameters || {}) })));
+        channel.send([0]); // message separator
+    });
+}
+
+/**
+ * Do a varlink call on a new channel. This is more expensive than
+ * `varlinkCallChannel()` but allows multiple parallel calls.
+ */
+function varlinkCall(channelOptions, method, parameters) {
+    var channel = cockpit.channel(Object.assign({payload: "stream", binary: true}, channelOptions));
+    var response = varlinkCallChannel(channel, method, parameters);
+    response.finally(() => channel.close());
+    return response;
+}
 
 export class Application extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
-        cockpit.file('/etc/hostname').read().done((content) => {
-            this.setState({ 'hostname': content.trim() });
-        });
+        this.state = { version: { version: "unknown" }, images: [], containers: [] };
+
+        varlinkCall(PODMAN, "io.projectatomic.podman.GetVersion")
+            .then(reply => this.setState({ version: reply.version }))
+            .catch(ex => console.error("Failed to do GetVersion call:", JSON.stringify(ex)));
+
+        varlinkCall(PODMAN, "io.projectatomic.podman.ListImages")
+            .then(reply => this.setState({ images: reply.images }))
+            .catch(ex => console.error("Failed to do ListImages call:", JSON.stringify(ex)));
+
+        varlinkCall(PODMAN, "io.projectatomic.podman.ListContainers")
+            .then(reply => this.setState({ containers: reply.containers || [] }))
+            .catch(ex => console.error("Failed to do ListContainers call:", JSON.stringify(ex), ex.toString()));
     }
 
     render() {
+        let images = this.state.images.map(image => <li>{ image.repoTags.join(", ") } (created: {image.created})</li>);
+        let containers = this.state.containers.map(container => <li>{ container.command.join(", ")} </li> );
+
         return (
             <div className="container-fluid">
-                <h2>Starter Kit</h2>
-                <p>
-                    { cockpit.format(_("Running on $0"), this.state.hostname) }
-                </p>
+                <h2>Podman Varlink Demo</h2>
+                <div>
+                    <span>podman version: {this.state.version.version}</span>
+                </div>
+
+                <h3>Images</h3>
+                <ul>
+                    {images}
+                </ul>
+                <h3>Containers</h3>
+                <ul>
+                    {containers}
+                </ul>
             </div>
         );
     }
