86f023b9044c5f1b821d8cdceef7eb436e398af9
Qi Wang <qiwan@redhat.com>
2018-10-17 12:38:27 -0400

    Remove react-modal & reactstrap from package.json

--- src/ContainersRunImageModal.jsx
+++ src/ContainersRunImageModal.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Modal from 'react-modal';
+import {Button, Modal} from 'patternfly-react';
 
 const ContainersRunImageModal = (props) => {
     return (
-        <Modal isOpen={props.show}>
+        <Modal show={props.show}>
             <h4>Test</h4>
-            <button onClick={props.handleCancelRunImage}>Cancel</button>
+            <Button onClick={props.handleCancelRunImage}>Cancel</Button>
         </Modal>
     );
 };
--- src/ImageRemoveErrorModal.jsx
+++ src/ImageRemoveErrorModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal } from 'patternfly-react';
 import cockpit from 'cockpit';
 
 const _ = cockpit.gettext;
@@ -8,17 +8,17 @@ const ImageRemoveErrorModal = (props) => {
     const repoTag = props.imageWillDelete.RepoTags ? _(props.imageWillDelete.RepoTags[0]) : _("");
     return (
         <div>
-            <Modal isOpen={props.setImageRemoveErrorModal} fade={false} >
-                <ModalHeader>
-                    {cockpit.format(_("Please confirm forced deletion of $0"), _(repoTag))}
-                </ModalHeader>
-                <ModalBody>
+            <Modal show={props.setImageRemoveErrorModal} >
+                <Modal.Header>
+                    <Modal.Title>{cockpit.format(_("Please confirm forced deletion of $0"), _(repoTag))}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
                     {_(props.imageRemoveErrorMsg)}
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="secondary" onClick={props.handleCancelImageRemoveError}>{_("Cancel")}</Button>
-                    <Button color="danger" onClick={props.handleForceRemoveImage}>{_("Force Delete")}</Button>{' '}
-                </ModalFooter>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={props.handleCancelImageRemoveError}>{_("Cancel")}</Button>
+                    <Button bsStyle="danger" onClick={props.handleForceRemoveImage}>{_("Force Delete")}</Button>
+                </Modal.Footer>
             </Modal>
         </div>
     );
