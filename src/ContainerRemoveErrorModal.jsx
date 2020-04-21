3a577322b108bfbc287f06439991c1f1fe9c8da0
Katerina Koukiou <kkoukiou@redhat.com>
2020-04-21 11:42:31 +0200

    Disable button and show a spinner while delete operation is in progress

--- src/ContainerRemoveErrorModal.jsx
+++ src/ContainerRemoveErrorModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from 'patternfly-react';
 import { Button } from '@patternfly/react-core';
 import cockpit from 'cockpit';
@@ -7,9 +7,10 @@ const _ = cockpit.gettext;
 
 const ContainerRemoveErrorModal = (props) => {
     const name = props.containerWillDelete ? _(props.containerWillDelete.Name) : "";
+    const [inProgress, setInProgress] = useState(false);
     return (
         <div>
-            <Modal show={props.setContainerRemoveErrorModal}>
+            <Modal key={name} show={props.setContainerRemoveErrorModal}>
                 <Modal.Header>
                     <Modal.Title>{cockpit.format(_("Please confirm forced deletion of $0"), name)}</Modal.Title>
                 </Modal.Header>
@@ -17,8 +18,11 @@ const ContainerRemoveErrorModal = (props) => {
                     {_("Container is currently running.")}
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="danger" className="btn-ctr-forcedelete" onClick={props.handleForceRemoveContainer}>{_("Force Delete")}</Button>
+                    <Button variant="danger" isDisabled={inProgress} className="btn-ctr-forcedelete" onClick={() => {
+                        setInProgress(true); props.handleForceRemoveContainer().finally(() => setInProgress(false));
+                    }}>{_("Force Delete")}</Button>
                     <Button variant="link" onClick={props.handleCancelRemoveError}>{_("Cancel")}</Button>
+                    {inProgress && <div className="spinner spinner-sm pull-right" />}
                 </Modal.Footer>
             </Modal>
         </div>
