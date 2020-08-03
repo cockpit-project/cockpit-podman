cockpit-project/cockpit-podman

commit b91d6906ef5f27cfaf06c7f035096181b2150b55

Dynamically update containers' memory and cpu info
Closes #431
Fixes #376

src/app.jsx
@@ -121,17 +121,17 @@ class Application extends React.Component {
     }
 
     updateContainerStats(id, system) {
-        client.getContainerStats(system, id)
-                .then(reply => {
-                    this.updateState("containersStats", reply.Id + system.toString(), reply);
-                })
-                .catch(ex => {
-                    if (ex.error === "io.podman.ErrRequiresCgroupsV2ForRootless") { // TODO - Check if this message still applies
-                        console.log("This OS does not support CgroupsV2. Some information may be missing.");
-                        this.updateState("containersStats", id + system.toString(), -1);
-                    } else
-                        console.warn("Failed to update container stats:", JSON.stringify(ex));
-                });
+        client.getContainerStats(system, id, reply => {
+            if (reply.response) // executed when container stop, with reply: {cause, message, response}
+                console.warn("Failed to update container stats:", JSON.stringify(reply.message));
+            else
+                this.updateState("containersStats", reply.Id + system.toString(), reply);
+        }).catch(ex => {
+            if (ex.cause == "no support for CGroups V1 in rootless environments") {
+                console.log("This OS does not support CgroupsV2. Some information may be missing.");
+            } else
+                console.warn("Failed to update container stats:", JSON.stringify(ex.message));
+        });
     }
 
     updateContainersAfterEvent(system) {

test/check-application
@@ -528,6 +528,14 @@ class TestApplication(testlib.MachineCase):
             full = numbers[1].strip().split()
             self.assertTrue(full[0].replace('.', '', 1).isdigit())
             self.assertIn(full[1], ["GiB", "MiB"])
+
+            # Test that the value is updated dynamically
+            self.execute(auth, "podman exec -i swamped-crate dd bs=1024 count=1000000 < /dev/urandom > outfile")
+            new_cpu = b.text("#containers-containers tbody tr:contains('busybox:latest') > td:nth-child(3)")
+            self.assertIn('%', new_cpu)
+            new_num = new_cpu[:-1]
+            self.assertTrue(new_num.replace('.', '', 1).isdigit())
+            self.assertTrue(new_num > num)
         else:
             # No support for CGroupsV2
             self.assertEqual(cpu, "n/a")

src/client.js
@@ -63,14 +63,13 @@ export function getContainers(system, id) {
     });
 }
 
-export function getContainerStats(system, id) {
+export function getContainerStats(system, id, callback) {
     return new Promise((resolve, reject) => {
         const options = {
-            stream: false, // FIXME that it actually streams
+            stream: true,
         };
-        podmanCall("libpod/containers/" + id + "/stats", "GET", options, system)
-                .then(reply => resolve(JSON.parse(reply)))
-                .catch(reject);
+        podmanMonitor("libpod/containers/" + id + "/stats", "GET", options, callback, system)
+                .then(resolve, reject);
     });
 }
 

commit 2830cf6b9bdf06245e2ad1ca05e7fd3d7bb8a150

Fix showing 'n/a' on stats when on CGroupsV1
This got broken with commit b91d6906

src/Containers.jsx
@@ -129,8 +129,8 @@ class Containers extends React.Component {
         let proc = "";
         let mem = "";
         if (containerStats) {
-            proc = containerStats.cpu_stats ? containerStats.cpu_stats.cpu.toFixed(2) + "%" : <abbr title={_("not available")}>{_("n/a")}</abbr>;
-            mem = containerStats.memory_stats ? utils.format_memory_and_limit(containerStats.memory_stats.usage, containerStats.memory_stats.limit) : <abbr title={_("not available")}>{_("n/a")}</abbr>;
+            proc = containerStats.cpu_stats ? containerStats.cpu_stats.cpu.toFixed(2) + "%" : <div><abbr title={_("not available")}>{_("n/a")}</abbr></div>;
+            mem = containerStats.memory_stats ? utils.format_memory_and_limit(containerStats.memory_stats.usage, containerStats.memory_stats.limit) : <div><abbr title={_("not available")}>{_("n/a")}</abbr></div>;
         }
         const info_block =
             <div className="container-block">

src/ImageUsedBy.jsx
@@ -11,8 +11,8 @@ const renderRow = (containerStats, container) => {
     let proc = "";
     let mem = "";
     if (containerStats) {
-        proc = containerStats.cpu_stats ? containerStats.cpu_stats.cpu.toFixed(2) + "%" : <abbr title={_("not available")}>{_("n/a")}</abbr>;
-        mem = containerStats.memory_stats ? utils.format_memory_and_limit(containerStats.memory_stats.usage, containerStats.memory_stats.limit) : <abbr title={_("not available")}>{_("n/a")}</abbr>;
+        proc = containerStats.cpu_stats ? containerStats.cpu_stats.cpu.toFixed(2) + "%" : <div><abbr title={_("not available")}>{_("n/a")}</abbr></div>;
+        mem = containerStats.memory_stats ? utils.format_memory_and_limit(containerStats.memory_stats.usage, containerStats.memory_stats.limit) : <div><abbr title={_("not available")}>{_("n/a")}</abbr></div>;
     }
     const columns = [
         { title: container.Names, header: true },

src/app.jsx
@@ -139,6 +139,7 @@ class Application extends React.Component {
         }).catch(ex => {
             if (ex.cause == "no support for CGroups V1 in rootless environments") {
                 console.log("This OS does not support CgroupsV2. Some information may be missing.");
+                this.updateState("containersStats", id + system.toString(), -1);
             } else
                 console.warn("Failed to update container stats:", JSON.stringify(ex.message));
         });

test/check-application
@@ -75,8 +75,7 @@ class TestApplication(testlib.MachineCase):
         self.has_selinux = self.machine.image not in ["debian-testing"]
 
     def cgroupsV2(self):
-        # We will introduce images that are still cgroupsV1 later on
-        return True
+        return self.machine.image not in ["debian-testing"]
 
     def execute(self, system, cmd):
         if system: