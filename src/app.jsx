75fc8b57fa7a46b0cc74d2007e911fa64e23d6d4
Matej Marusak <mmarusak@redhat.com>
2020-10-14 10:34:46 +0200

    Listen on event for image build
    In https://github.com/containers/podman/pull/7202 a new event was
    introduced. Listen on it as well.
    
    Closes #507

--- src/app.jsx
+++ src/app.jsx
@@ -328,6 +328,7 @@ class Application extends React.Component {
         case 'untag':
         case 'remove':
         case 'prune':
+        case 'build':
             this.updateImagesAfterEvent(system);
             break;
         default:
