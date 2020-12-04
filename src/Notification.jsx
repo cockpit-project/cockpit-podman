f5af1dd24344fa165cc9e297912f4d39c10e3c15
Marius Vollmer <mvollmer@redhat.com>
2020-12-04 10:00:46 +0200

    notifications: Log errors in the Browser console, once

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -23,7 +23,19 @@ import cockpit from 'cockpit';
 
 const _ = cockpit.gettext;
 
+let last_error = "";
+
+function log_error_if_changed(error) {
+    // Put the error in the browser log, for easier debugging and
+    // matching of known issues in the integration tests.
+    if (error != last_error) {
+        last_error = error;
+        console.error(error);
+    }
+}
+
 export const ErrorNotification = ({ errorMessage, errorDetail, onDismiss }) => {
+    log_error_if_changed(errorMessage + (errorDetail ? ": " + errorDetail : ""));
     return (
         <Alert isInline variant='danger' title={errorMessage}
             actionClose={onDismiss ? <AlertActionCloseButton onClose={onDismiss} /> : null}>
