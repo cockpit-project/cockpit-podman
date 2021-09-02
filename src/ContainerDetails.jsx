bf1de36cc554c826295a4d777064d859b675ee92
Katerina Koukiou <kkoukiou@redhat.com>
2021-09-02 13:45:43 +0200

    Do not show labels in container details if their values are empty

--- src/ContainerDetails.jsx
+++ src/ContainerDetails.jsx
@@ -25,52 +25,55 @@ const render_container_published_ports = (ports) => {
     return result;
 };
 
-// container.Created is an ISO datetime string, like 2021-07-11T14:17:00.350804119+02:00
-const ContainerDetails = ({ container, containerDetail }) => (
-    <Flex spaceItems={{ modifier: 'spaceItemsXl' }}>
-        <DescriptionList className='container-details-basic'>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("ID")}</DescriptionListTerm>
-                <DescriptionListDescription>{container.Id}</DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("Image")}</DescriptionListTerm>
-                <DescriptionListDescription>{container.Image}</DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("Command")}</DescriptionListTerm>
-                <DescriptionListDescription>{container.Command ? utils.quote_cmdline(container.Command) : ""}</DescriptionListDescription>
-            </DescriptionListGroup>
-        </DescriptionList>
-        <DescriptionList columnModifier={{ default: '2Col' }} className='container-details-networking'>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("Ports")}</DescriptionListTerm>
-                <DescriptionListDescription>{render_container_published_ports(container.Ports)}</DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("IP address")}</DescriptionListTerm>
-                <DescriptionListDescription>{containerDetail ? containerDetail.NetworkSettings.IPAddress : ""}</DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("Gateway")}</DescriptionListTerm>
-                <DescriptionListDescription>{containerDetail ? containerDetail.NetworkSettings.Gateway : ""}</DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("MAC address")}</DescriptionListTerm>
-                <DescriptionListDescription>{containerDetail ? containerDetail.NetworkSettings.MacAddress : ""}</DescriptionListDescription>
-            </DescriptionListGroup>
-        </DescriptionList>
-        <DescriptionList className='container-details-state'>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("Created")}</DescriptionListTerm>
-                <DescriptionListDescription>{utils.localize_time(Date.parse(container.Created) / 1000)}</DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-                <DescriptionListTerm>{_("State")}</DescriptionListTerm>
-                <DescriptionListDescription>{render_container_state(container)}</DescriptionListDescription>
-            </DescriptionListGroup>
-        </DescriptionList>
-    </Flex>
-);
+const ContainerDetails = ({ container, containerDetail }) => {
+    const ports = render_container_published_ports(container.Ports);
+
+    return (
+        <Flex spaceItems={{ modifier: 'spaceItemsXl' }}>
+            <DescriptionList className='container-details-basic'>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>{_("ID")}</DescriptionListTerm>
+                    <DescriptionListDescription>{container.Id}</DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>{_("Image")}</DescriptionListTerm>
+                    <DescriptionListDescription>{container.Image}</DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>{_("Command")}</DescriptionListTerm>
+                    <DescriptionListDescription>{container.Command ? utils.quote_cmdline(container.Command) : ""}</DescriptionListDescription>
+                </DescriptionListGroup>
+            </DescriptionList>
+            <DescriptionList columnModifier={{ default: '2Col' }} className='container-details-networking'>
+                {ports.length > 0 && <DescriptionListGroup>
+                    <DescriptionListTerm>{_("Ports")}</DescriptionListTerm>
+                    <DescriptionListDescription>{ports}</DescriptionListDescription>
+                </DescriptionListGroup>}
+                {containerDetail && containerDetail.NetworkSettings.IPAddress && <DescriptionListGroup>
+                    <DescriptionListTerm>{_("IP address")}</DescriptionListTerm>
+                    <DescriptionListDescription>{containerDetail.NetworkSettings.IPAddress}</DescriptionListDescription>
+                </DescriptionListGroup>}
+                {containerDetail && containerDetail.NetworkSettings.Gateway && <DescriptionListGroup>
+                    <DescriptionListTerm>{_("Gateway")}</DescriptionListTerm>
+                    <DescriptionListDescription>{containerDetail.NetworkSettings.Gateway}</DescriptionListDescription>
+                </DescriptionListGroup>}
+                {containerDetail && containerDetail.NetworkSettings.MacAddress && <DescriptionListGroup>
+                    <DescriptionListTerm>{_("MAC address")}</DescriptionListTerm>
+                    <DescriptionListDescription>{containerDetail.NetworkSettings.MacAddress}</DescriptionListDescription>
+                </DescriptionListGroup>}
+            </DescriptionList>
+            <DescriptionList className='container-details-state'>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>{_("Created")}</DescriptionListTerm>
+                    <DescriptionListDescription>{utils.localize_time(Date.parse(container.Created) / 1000)}</DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>{_("State")}</DescriptionListTerm>
+                    <DescriptionListDescription>{render_container_state(container)}</DescriptionListDescription>
+                </DescriptionListGroup>
+            </DescriptionList>
+        </Flex>
+    );
+};
 
 export default ContainerDetails;
