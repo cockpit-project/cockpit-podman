584165af32f047591145b845da81761ab54b1e92
Matej Marusak <mmarusak@redhat.com>
2022-02-04 09:42:13 +0100

    Use default flex spacing
    Our own defined is equal to the default one once we use `<FlexItem>`s.

--- src/ContainerDetails.jsx
+++ src/ContainerDetails.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cockpit from 'cockpit';
 import * as utils from './util.js';
 
-import { DescriptionList, DescriptionListTerm, DescriptionListDescription, DescriptionListGroup, Flex, List, ListItem } from "@patternfly/react-core";
+import { DescriptionList, DescriptionListTerm, DescriptionListDescription, DescriptionListGroup, Flex, List, FlexItem, ListItem } from "@patternfly/react-core";
 
 const _ = cockpit.gettext;
 
@@ -46,49 +46,55 @@ const ContainerDetails = ({ container, containerDetail }) => {
     );
 
     return (
-        <Flex spaceItems={{ modifier: 'spaceItemsXl' }}>
-            <DescriptionList className='container-details-basic'>
-                <DescriptionListGroup>
-                    <DescriptionListTerm>{_("ID")}</DescriptionListTerm>
-                    <DescriptionListDescription>{utils.truncate_id(container.Id)}</DescriptionListDescription>
-                </DescriptionListGroup>
-                <DescriptionListGroup>
-                    <DescriptionListTerm>{_("Image")}</DescriptionListTerm>
-                    <DescriptionListDescription>{container.Image}</DescriptionListDescription>
-                </DescriptionListGroup>
-                <DescriptionListGroup>
-                    <DescriptionListTerm>{_("Command")}</DescriptionListTerm>
-                    <DescriptionListDescription>{container.Command ? utils.quote_cmdline(container.Command) : ""}</DescriptionListDescription>
-                </DescriptionListGroup>
-            </DescriptionList>
-            {networkOptions && <DescriptionList columnModifier={{ default: '2Col' }} className='container-details-networking'>
-                {ports && <DescriptionListGroup>
-                    <DescriptionListTerm>{_("Ports")}</DescriptionListTerm>
-                    <DescriptionListDescription>{ports}</DescriptionListDescription>
-                </DescriptionListGroup>}
-                {containerDetail && containerDetail.NetworkSettings.IPAddress && <DescriptionListGroup>
-                    <DescriptionListTerm>{_("IP address")}</DescriptionListTerm>
-                    <DescriptionListDescription>{containerDetail.NetworkSettings.IPAddress}</DescriptionListDescription>
-                </DescriptionListGroup>}
-                {containerDetail && containerDetail.NetworkSettings.Gateway && <DescriptionListGroup>
-                    <DescriptionListTerm>{_("Gateway")}</DescriptionListTerm>
-                    <DescriptionListDescription>{containerDetail.NetworkSettings.Gateway}</DescriptionListDescription>
-                </DescriptionListGroup>}
-                {containerDetail && containerDetail.NetworkSettings.MacAddress && <DescriptionListGroup>
-                    <DescriptionListTerm>{_("MAC address")}</DescriptionListTerm>
-                    <DescriptionListDescription>{containerDetail.NetworkSettings.MacAddress}</DescriptionListDescription>
-                </DescriptionListGroup>}
-            </DescriptionList>}
-            <DescriptionList className='container-details-state'>
-                <DescriptionListGroup>
-                    <DescriptionListTerm>{_("Created")}</DescriptionListTerm>
-                    <DescriptionListDescription>{utils.localize_time(Date.parse(container.Created) / 1000)}</DescriptionListDescription>
-                </DescriptionListGroup>
-                <DescriptionListGroup>
-                    <DescriptionListTerm>{_("State")}</DescriptionListTerm>
-                    <DescriptionListDescription>{render_container_state(container)}</DescriptionListDescription>
-                </DescriptionListGroup>
-            </DescriptionList>
+        <Flex>
+            <FlexItem>
+                <DescriptionList className='container-details-basic'>
+                    <DescriptionListGroup>
+                        <DescriptionListTerm>{_("ID")}</DescriptionListTerm>
+                        <DescriptionListDescription>{utils.truncate_id(container.Id)}</DescriptionListDescription>
+                    </DescriptionListGroup>
+                    <DescriptionListGroup>
+                        <DescriptionListTerm>{_("Image")}</DescriptionListTerm>
+                        <DescriptionListDescription>{container.Image}</DescriptionListDescription>
+                    </DescriptionListGroup>
+                    <DescriptionListGroup>
+                        <DescriptionListTerm>{_("Command")}</DescriptionListTerm>
+                        <DescriptionListDescription>{container.Command ? utils.quote_cmdline(container.Command) : ""}</DescriptionListDescription>
+                    </DescriptionListGroup>
+                </DescriptionList>
+            </FlexItem>
+            <FlexItem>
+                {networkOptions && <DescriptionList columnModifier={{ default: '2Col' }} className='container-details-networking'>
+                    {ports && <DescriptionListGroup>
+                        <DescriptionListTerm>{_("Ports")}</DescriptionListTerm>
+                        <DescriptionListDescription>{ports}</DescriptionListDescription>
+                    </DescriptionListGroup>}
+                    {containerDetail && containerDetail.NetworkSettings.IPAddress && <DescriptionListGroup>
+                        <DescriptionListTerm>{_("IP address")}</DescriptionListTerm>
+                        <DescriptionListDescription>{containerDetail.NetworkSettings.IPAddress}</DescriptionListDescription>
+                    </DescriptionListGroup>}
+                    {containerDetail && containerDetail.NetworkSettings.Gateway && <DescriptionListGroup>
+                        <DescriptionListTerm>{_("Gateway")}</DescriptionListTerm>
+                        <DescriptionListDescription>{containerDetail.NetworkSettings.Gateway}</DescriptionListDescription>
+                    </DescriptionListGroup>}
+                    {containerDetail && containerDetail.NetworkSettings.MacAddress && <DescriptionListGroup>
+                        <DescriptionListTerm>{_("MAC address")}</DescriptionListTerm>
+                        <DescriptionListDescription>{containerDetail.NetworkSettings.MacAddress}</DescriptionListDescription>
+                    </DescriptionListGroup>}
+                </DescriptionList>}
+            </FlexItem>
+            <FlexItem>
+                <DescriptionList className='container-details-state'>
+                    <DescriptionListGroup>
+                        <DescriptionListTerm>{_("Created")}</DescriptionListTerm>
+                        <DescriptionListDescription>{utils.localize_time(Date.parse(container.Created) / 1000)}</DescriptionListDescription>
+                    </DescriptionListGroup>
+                    <DescriptionListGroup>
+                        <DescriptionListTerm>{_("State")}</DescriptionListTerm>
+                        <DescriptionListDescription>{render_container_state(container)}</DescriptionListDescription>
+                    </DescriptionListGroup>
+                </DescriptionList>
+            </FlexItem>
         </Flex>
     );
 };
