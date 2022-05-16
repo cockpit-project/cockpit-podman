b05e1f5e62d1fa9fafa758595256530d9678994a
Jelle van der Waa <jvanderwaa@redhat.com>
2022-05-16 15:14:31 +0200

    Support url parameters for filtering
    Since Cockpit 269 the service top CPU usage lists podman containers and
    links to the podman page without filtering on a specific container name.
    For users it would be more useful if they could directly spot the
    container, the newly introduced url parameters can filter based on
    owner, a search string and on the container running state.

--- src/ContainerHeader.jsx
+++ src/ContainerHeader.jsx
@@ -6,60 +6,33 @@ import {
 } from '@patternfly/react-core';
 const _ = cockpit.gettext;
 
-class ContainerHeader extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            owner: 'all',
-            filterText: ''
-        };
-        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-        this.handleOwnerChange = this.handleOwnerChange.bind(this);
-    }
-
-    filterChanged() {
-        if (this.props.onFilterChanged)
-            this.props.onFilterChanged(this.state.filterText);
-    }
-
-    handleOwnerChange (value) {
-        this.setState({ owner: value });
-        if (this.props.onOwnerChanged) {
-            this.props.onOwnerChanged(value);
-        }
-    }
-
-    handleFilterTextChange(value) {
-        this.setState({ filterText: value }, this.filterChanged);
-    }
-
-    render() {
-        return (
-            <Toolbar className="pf-m-page-insets">
-                <ToolbarContent>
-                    { this.props.twoOwners &&
+const ContainerHeader = ({ user, twoOwners, ownerFilter, handleOwnerChanged, textFilter, handleFilterChanged }) => {
+    return (
+        <Toolbar className="pf-m-page-insets">
+            <ToolbarContent>
+                { twoOwners &&
                     <>
                         <ToolbarItem variant="label">
                             {_("Owner")}
                         </ToolbarItem>
                         <ToolbarItem>
-                            <FormSelect id="containers-containers-owner" value={this.state.owner} onChange={this.handleOwnerChange}>
-                                <FormSelectOption value='user' label={this.props.user} />
+                            <FormSelect id="containers-containers-owner" value={ownerFilter} onChange={handleOwnerChanged}>
+                                <FormSelectOption value='user' label={user} />
                                 <FormSelectOption value='system' label={_("System")} />
                                 <FormSelectOption value='all' label={_("All")} />
                             </FormSelect>
                         </ToolbarItem>
                     </>
-                    }
-                    <ToolbarItem>
-                        <TextInput id="containers-filter"
+                }
+                <ToolbarItem>
+                    <TextInput id="containers-filter"
                                    placeholder={_("Type to filter…")}
-                                   onChange={this.handleFilterTextChange} />
-                    </ToolbarItem>
-                </ToolbarContent>
-            </Toolbar>
-        );
-    }
-}
+                                   value={textFilter}
+                                   onChange={handleFilterChanged} />
+                </ToolbarItem>
+            </ToolbarContent>
+        </Toolbar>
+    );
+};
 
 export default ContainerHeader;
