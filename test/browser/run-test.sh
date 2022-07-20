cockpit-project/cockpit-podman

commit c22bb7121cd73324ebda7dcf2cae6a96bc25efe0

fmf: Ensure current node_modules/
Inexplicably and infuriatingly, TF test runs have an ancient existing
node_modules/ directory on the testing farm. It's not from anything that
we provide in the dist tarballs, packit, spec, or SRPM, but its
chrome-remote-interface is too old to run with current Firefox.

test/browser/run-test.sh
@@ -7,15 +7,9 @@ git init
 rm -f bots  # common local case: existing bots symlink
 make bots test/common
 
-# support running from clean git tree
-if [ ! -d node_modules/chrome-remote-interface ]; then
-    # copy package.json temporarily otherwise npm might try to install the dependencies from it
-    rm -f package-lock.json  # otherwise the command below installs *everything*, argh
-    mv package.json .package.json
-    # only install a subset to save time/space
-    npm install chrome-remote-interface sizzle
-    mv .package.json package.json
-fi
+# ensure current node_modules for testing
+rm -rf node_modules
+tools/node-modules checkout
 
 # disable detection of affected tests; testing takes too long as there is no parallelization
 mv .git dot-git

commit 9c74c2b52bcf772e917d965dd11496a3732e970b

fmf: Add non-git fallback for npm module installation
Commit c22bb7121cd73324e broke running tests for tarball releases [1].
Bring back the `npm install` for the non-git case.

Clean up the pointless `git init` and make the dot-git moving
conditional on the presence of .git (i.e. testing an upstream PR).

[1] https://src.fedoraproject.org/rpms/cockpit-podman/pull-request/53

test/browser/run-test.sh
@@ -3,16 +3,24 @@ set -eux
 
 # tests need cockpit's bots/ libraries and test infrastructure
 cd $SOURCE
-git init
 rm -f bots  # common local case: existing bots symlink
 make bots test/common
 
 # ensure current node_modules for testing
 rm -rf node_modules
-tools/node-modules checkout
+if [ -e .git ]; then
+    tools/node-modules checkout
+    # disable detection of affected tests; testing takes too long as there is no parallelization
+    mv .git dot-git
 
-# disable detection of affected tests; testing takes too long as there is no parallelization
-mv .git dot-git
+else
+    # move package.json temporarily, otherwise npm might try to install the dependencies from it
+    rm -f package-lock.json  # otherwise the command below installs *everything*, argh
+    mv package.json .package.json
+    # only install a subset to save time/space
+    npm install chrome-remote-interface sizzle
+    mv .package.json package.json
+fi
 
 . /etc/os-release
 export TEST_OS="${ID}-${VERSION_ID/./-}"
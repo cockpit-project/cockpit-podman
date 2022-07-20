9c74c2b52bcf772e917d965dd11496a3732e970b
Martin Pitt <martin@piware.de>
2022-07-20 14:38:09 +0200

    fmf: Add non-git fallback for npm module installation
    Commit c22bb7121cd73324e broke running tests for tarball releases [1].
    Bring back the `npm install` for the non-git case.
    
    Clean up the pointless `git init` and make the dot-git moving
    conditional on the presence of .git (i.e. testing an upstream PR).
    
    [1] https://src.fedoraproject.org/rpms/cockpit-podman/pull-request/53

--- test/browser/run-test.sh
+++ test/browser/run-test.sh
@@ -3,16 +3,24 @@ set -eux
 
 # tests need cockpit's bots/ libraries and test infrastructure
 cd $SOURCE
-git init
 rm -f bots  # common local case: existing bots symlink
 make bots test/common
 
 # ensure current node_modules for testing
 rm -rf node_modules
-tools/node-modules checkout
+if [ -e .git ]; then
+    tools/node-modules checkout
+    # disable detection of affected tests; testing takes too long as there is no parallelization
+    mv .git dot-git
 
-# disable detection of affected tests; testing takes too long as there is no parallelization
-mv .git dot-git
+else
+    # move package.json temporarily, otherwise npm might try to install the dependencies from it
+    rm -f package-lock.json  # otherwise the command below installs *everything*, argh
+    mv package.json .package.json
+    # only install a subset to save time/space
+    npm install chrome-remote-interface sizzle
+    mv .package.json package.json
+fi
 
 . /etc/os-release
 export TEST_OS="${ID}-${VERSION_ID/./-}"
