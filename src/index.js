484ea9b41000a1321200abafb1f6e048f8273992
Katerina Koukiou <kkoukiou@redhat.com>
2022-12-14 17:30:49 +0100

    Bring in tabular fonts and bump cockpit lib

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
+import "cockpit-dark-theme";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'patternfly/patternfly-4-cockpit.scss';
