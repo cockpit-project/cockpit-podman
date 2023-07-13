b001cb4f8ad65df83e6dd4f622f190d5a74ef83b
Simon Kobyda <skobyda@redhat.com>
2023-07-13 11:37:58 +0200

    Validate volumes fields in Create container dialog

--- src/Volume.jsx
+++ src/Volume.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
 import { Checkbox } from "@patternfly/react-core/dist/esm/components/Checkbox";
 import { FormGroup } from "@patternfly/react-core/dist/esm/components/Form";
+import { FormHelper } from "cockpit-components-form-helper.jsx";
 import { FormSelect, FormSelectOption } from "@patternfly/react-core/dist/esm/components/FormSelect";
 import { Grid } from "@patternfly/react-core/dist/esm/layouts/Grid";
 import { TextInput } from "@patternfly/react-core/dist/esm/components/TextInput";
@@ -9,20 +10,56 @@ import { TrashIcon } from '@patternfly/react-icons';
 import { FileAutoComplete } from 'cockpit-components-file-autocomplete.jsx';
 import cockpit from 'cockpit';
 
+import * as utils from './util.js';
+
 const _ = cockpit.gettext;
 
-export const Volume = ({ id, item, onChange, idx, removeitem, additem, options, itemCount }) =>
+export function validateVolume(value, key) {
+    switch (key) {
+    case "hostPath":
+        break;
+    case "containerPath":
+        if (!value)
+            return _("Container path must not be empty");
+
+        break;
+    default:
+        console.error(`Unknown key "${key}"`); // not-covered: unreachable assertion
+    }
+}
+
+export const Volume = ({ id, item, onChange, idx, removeitem, additem, options, itemCount, validationFailed, onValidationChange }) =>
     (
         <Grid hasGutter id={id}>
-            <FormGroup className="pf-m-4-col-on-md" label={_("Host path")} fieldId={id + "-host-path"}>
+            <FormGroup className="pf-m-4-col-on-md"
+                id={id + "-host-path-group"}
+                label={_("Host path")}
+                fieldId={id + "-host-path"}
+            >
                 <FileAutoComplete id={id + "-host-path"}
                     value={item.hostPath || ''}
-                    onChange={ value => onChange(idx, 'hostPath', value) } />
+                    onChange={value => {
+                        utils.validationClear(validationFailed, "hostPath", onValidationChange);
+                        utils.validationDebounce(() => onValidationChange({ ...validationFailed, hostPath: validateVolume(value, "hostPath") }));
+                        onChange(idx, 'hostPath', value);
+                    }} />
+                <FormHelper helperTextInvalid={validationFailed?.hostPath} />
             </FormGroup>
-            <FormGroup className="pf-m-3-col-on-md" label={_("Container path")} fieldId={id + "-container-path"}>
+            <FormGroup className="pf-m-3-col-on-md"
+                id={id + "-container-path-group"}
+                label={_("Container path")}
+                fieldId={id + "-container-path"}
+                isRequired
+            >
                 <TextInput id={id + "-container-path"}
                     value={item.containerPath || ''}
-                    onChange={(_event, value) => onChange(idx, 'containerPath', value)} />
+                    validated={validationFailed?.containerPath ? "error" : "default"}
+                    onChange={(_event, value) => {
+                        utils.validationClear(validationFailed, "containerPath", onValidationChange);
+                        utils.validationDebounce(() => onValidationChange({ ...validationFailed, containerPath: validateVolume(value, "containerPath") }));
+                        onChange(idx, 'containerPath', value);
+                    }} />
+                <FormHelper helperTextInvalid={validationFailed?.containerPath} />
             </FormGroup>
             <FormGroup className="pf-m-2-col-on-md" label={_("Mode")} fieldId={id + "-mode"}>
                 <Checkbox id={id + "-mode"}
