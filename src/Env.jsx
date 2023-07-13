a0280962b605792ffcb37f2544617b3947545687
Simon Kobyda <skobyda@redhat.com>
2023-07-13 12:06:58 +0200

    Validate Environment variables fields in Create container dialog

--- src/Env.jsx
+++ src/Env.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
 import { FormGroup } from "@patternfly/react-core/dist/esm/components/Form";
+import { FormHelper } from "cockpit-components-form-helper.jsx";
 import { Grid } from "@patternfly/react-core/dist/esm/layouts/Grid";
 import { TextInput } from "@patternfly/react-core/dist/esm/components/TextInput";
 import { TrashIcon } from '@patternfly/react-icons';
 import cockpit from 'cockpit';
 
+import * as utils from './util.js';
+
 const _ = cockpit.gettext;
+
+export function validateEnvVar(env, key) {
+    switch (key) {
+    case "envKey":
+        if (!env)
+            return _("Key must not be empty");
+        break;
+    case "envValue":
+        break;
+    default:
+        console.error(`Unknown key "${key}"`); // not-covered: unreachable assertion
+    }
+}
 
 const handleEnvValue = (key, value, idx, onChange, additem, itemCount, companionField) => {
     // Allow the input of KEY=VALUE separated value pairs for bulk import only if the other
@@ -32,18 +48,40 @@ const handleEnvValue = (key, value, idx, onChange, additem, itemCount, companion
     }
 };
 
-export const EnvVar = ({ id, item, onChange, idx, removeitem, additem, itemCount }) =>
+export const EnvVar = ({ id, item, onChange, idx, removeitem, additem, itemCount, validationFailed, onValidationChange }) =>
     (
         <Grid hasGutter id={id}>
-            <FormGroup className="pf-m-6-col-on-md" label={_("Key")} fieldId={id + "-key-address"}>
+            <FormGroup className="pf-m-6-col-on-md"
+                id={id + "-key-group"}
+                label={_("Key")}
+                fieldId={id + "-key-address"}
+                isRequired
+            >
                 <TextInput id={id + "-key"}
                        value={item.envKey || ''}
-                       onChange={(_, value) => handleEnvValue('envKey', value, idx, onChange, additem, itemCount, item.envValue)} />
+                       validated={validationFailed?.envKey ? "error" : "default"}
+                       onChange={(_event, value) => {
+                           utils.validationClear(validationFailed, "envKey", onValidationChange);
+                           utils.validationDebounce(() => onValidationChange({ ...validationFailed, envKey: validateEnvVar(value, "envKey") }));
+                           handleEnvValue('envKey', value, idx, onChange, additem, itemCount, item.envValue);
+                       }} />
+                <FormHelper helperTextInvalid={validationFailed?.envKey} />
             </FormGroup>
-            <FormGroup className="pf-m-6-col-on-md" label={_("Value")} fieldId={id + "-value-address"}>
+            <FormGroup className="pf-m-6-col-on-md"
+                id={id + "-value-group"}
+                label={_("Value")}
+                fieldId={id + "-value-address"}
+                isRequired
+            >
                 <TextInput id={id + "-value"}
                        value={item.envValue || ''}
-                       onChange={(_, value) => handleEnvValue('envValue', value, idx, onChange, additem, itemCount, item.envKey)} />
+                       validated={validationFailed?.envValue ? "error" : "default"}
+                       onChange={(_event, value) => {
+                           utils.validationClear(validationFailed, "envValue", onValidationChange);
+                           utils.validationDebounce(() => onValidationChange({ ...validationFailed, envValue: validateEnvVar(value, "envValue") }));
+                           handleEnvValue('envValue', value, idx, onChange, additem, itemCount, item.envValue);
+                       }} />
+                <FormHelper helperTextInvalid={validationFailed?.envValue} />
             </FormGroup>
             <FormGroup className="pf-m-1-col-on-md remove-button-group">
                 <Button variant='plain'
