0c03211aa86ee86fca35bcd30789fa5387459fe9
Jelle van der Waa <jvanderwaa@redhat.com>
2024-04-03 17:19:43 +0200

    Pass all local images for container integration details
    On some systems a toolbox container has an intermediate image as image
    which since 17d258855fac34556d66337f9015d47741dd482c is no longer passed
    to the renderRow.
    
    The introduced test uncovered a race condition where deleting the image
    when running the container lead to `image` being undefined, this is now
    also handled.
    
    Closes #1649

--- src/ContainerIntegration.jsx
+++ src/ContainerIntegration.jsx
@@ -98,7 +98,10 @@ const ContainerIntegration = ({ container, localImages }) => {
     const volumes = renderContainerVolumes(container.Mounts);
 
     const image = localImages.filter(img => img.Id === container.Image)[0];
-    const env = <ContainerEnv containerEnv={container.Config.Env} imageEnv={image.Env} />;
+    let env = null;
+    // Podman allows one to remove an image while it has a container attached
+    if (image)
+        env = <ContainerEnv containerEnv={container.Config.Env} imageEnv={image.Env} />;
 
     return (
         <DescriptionList isAutoColumnWidths columnModifier={{ md: '3Col' }} className='container-integration'>
