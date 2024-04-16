9d0d845e77ac49f9d2b29522aa0dd2d0ce236c14
Martin Pitt <mpitt@redhat.com>
2024-04-16 12:02:12 +0200

    fmf: Restrict "-stream" TEST_OS suffix to CentOS 8/9
    Our c10s CI image is called "centos-10" without the -stream suffix. This
    fits better with `os-release` and makes the names shorter. Adjust the
    building of `$TEST_OS` accordingly.
    
    [1] https://github.com/cockpit-project/bots/pull/6218

--- test/browser/run-test.sh
+++ test/browser/run-test.sh
@@ -20,7 +20,7 @@ fi
 . /run/host/usr/lib/os-release
 export TEST_OS="${ID}-${VERSION_ID/./-}"
 
-if [ "${TEST_OS#centos-}" != "$TEST_OS" ]; then
+if [ "$TEST_OS" = "centos-8" ] || [ "$TEST_OS" = "centos-9" ]; then
     TEST_OS="${TEST_OS}-stream"
 fi
 
