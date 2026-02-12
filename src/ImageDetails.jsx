dd5f476cd20ee441b85c36cfd377db71822f42ea
Jelle van der Waa <jvanderwaa@redhat.com>
2026-02-12 18:24:07 +0100

    Show description, version and doc labels for images
    The OpenContainers Annotations Spec specifies what standardised labels
    may exist on a container image. For now expose the description,
    documentation link and version in the image details tab. This is
    particularly useful showing the version of the software you run when
    using the `latest` tag.

--- src/ImageDetails.jsx
+++ src/ImageDetails.jsx
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 import React from 'react';
 
+import { Button } from "@patternfly/react-core/dist/esm/components/Button";
 import { DescriptionList, DescriptionListDescription, DescriptionListGroup, DescriptionListTerm } from "@patternfly/react-core/dist/esm/components/DescriptionList";
 
 import cockpit from 'cockpit';
@@ -11,8 +12,31 @@ import * as utils from './util.js';
 const _ = cockpit.gettext;
 
 const ImageDetails = ({ containers, image, showAll }) => {
+    const labels = image?.Labels;
     return (
         <DescriptionList className='image-details' isAutoFit>
+            {labels?.["org.opencontainers.image.description"] &&
+            <DescriptionListGroup>
+                <DescriptionListTerm>{_("Description")}</DescriptionListTerm>
+                <DescriptionListDescription data-label="description">{labels["org.opencontainers.image.description"]}</DescriptionListDescription>
+            </DescriptionListGroup>
+            }
+            {labels?.["org.opencontainers.image.version"] &&
+            <DescriptionListGroup>
+                <DescriptionListTerm>{_("Version")}</DescriptionListTerm>
+                <DescriptionListDescription data-label="version">{labels["org.opencontainers.image.version"]}</DescriptionListDescription>
+            </DescriptionListGroup>
+            }
+            {labels?.["org.opencontainers.image.documentation"] &&
+            <DescriptionListGroup>
+                <DescriptionListTerm>{_("Links")}</DescriptionListTerm>
+                <DescriptionListDescription data-label="documentation">
+                    <Button variant="link" isInline component="a" href={labels["org.opencontainers.image.documentation"]} target="_blank noreferrer">
+                        {_("Documentation")}
+                    </Button>
+                </DescriptionListDescription>
+            </DescriptionListGroup>
+            }
             {image.Command &&
             <DescriptionListGroup>
                 <DescriptionListTerm>{_("Command")}</DescriptionListTerm>
